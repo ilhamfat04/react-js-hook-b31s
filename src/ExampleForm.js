626ffa00111bbfcae80dd5ebcc125b402b06e663
= <=>
2022-03-04 13:59:28 +0700

    update

--- src/ExampleForm.js
+++ src/ExampleForm.js
@@ -1,52 +1,67 @@
 //import useState here
-import { 
-        Container, 
-        Row, Col, Form, 
-        Button } from 'react-bootstrap'
+import { useState } from 'react'
+
+import {
+  Container,
+  Row, Col, Form,
+  Button
+} from 'react-bootstrap'
 
 function ExampleForm() {
   //create state with attribute fullname, email & password here..
+  const [state, setState] = useState({
+    fullname: "",
+    email: "",
+    password: ""
+  })
 
   const handleOnChange = (e) => {
     // setState here
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+
+    console.log(e.target.value);
   }
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
     //print state value with console.log here
+    console.log(state);
   }
 
-    return (
-      <Container>
-        <Row 
-          className="d-flex align-items-center justify-content-center vh-100">
-          <Col md="6">
+  return (
+    <Container>
+      <Row
+        className="d-flex align-items-center justify-content-center vh-100">
+        <Col md="6">
           <Form onSubmit={handleOnSubmit}>
             <div className="text-center h3">Register</div>
             <Form.Group className="mb-3" controlId="formFullName">
               <Form.Label>Full Name</Form.Label>
-              <Form.Control 
-                onChange={handleOnChange} 
+              <Form.Control
+                onChange={handleOnChange}
                 value={state.fullname}
-                name="fullname" size="sm" type="text" 
+                name="fullname" size="sm" type="text"
                 placeholder="Enter Full Name" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control 
-                onChange={handleOnChange} 
+              <Form.Control
+                onChange={handleOnChange}
                 value={state.email}
-                name="email" size="sm" type="email" 
+                name="email" size="sm" type="email"
                 placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control 
-                onChange={handleOnChange} 
+              <Form.Control
+                onChange={handleOnChange}
                 value={state.password}
-                name="password" size="sm" type="password" 
+                name="password" size="sm" type="password"
                 placeholder="Password" />
             </Form.Group>
 
@@ -54,10 +69,10 @@ function ExampleForm() {
               Submit
             </Button>
           </Form>
-          </Col>
-        </Row>
-      </Container>
-    )
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ExampleForm;
